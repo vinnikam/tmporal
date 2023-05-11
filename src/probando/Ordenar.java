b8688c6a80358de5dc9b27c197b5a1b68ee45795
hpclab <hpclab@172.20.9.227>
2023-05-11 18:43:59 -0500

    Adiciona funcionalidad

---
+++ src/probando/Ordenar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probando;
+
+/**
+ *
+ * @author hpclab
+ */
+public class Ordenar {
+    public static void main(String[] args) {
+        System.out.println("aljfdal");
+    }
+}
